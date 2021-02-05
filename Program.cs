40b3572ca44a3a255fec5b32e16bfe7123510d3c
Ensar Uç <69520181+ensaruc@users.noreply.github.com>
2021-02-05 14:36:17 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SartBloklari_SwitchCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kaçıncı ay doğdunuz?: ");
+            int ay = Convert.ToInt16(Console.ReadLine());
+
+            switch (ay)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Kış");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("İlkbahar");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Yaz");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("Sonbahar");
+                    break;
+                default:
+                    Console.WriteLine("Ay bilgisini yanlış girdiniz!");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
